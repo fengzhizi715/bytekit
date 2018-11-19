45595a9b5b88907c21b54eb7b60dd43e61a786d3
fengzhizi715 <fengzhizi715@126.com>
2018-11-19 20:41:04 +0800

    add Bytes

---
+++ core/src/main/java/com/safframework/bytes/Bytes.java
@@ -0,0 +1,25 @@
+package com.safframework.bytes;
+
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+/**
+ * Created by tony on 2018/11/19.
+ */
+public interface Bytes {
+
+    default boolean isEmpty() {
+        return size() == 0;
+    }
+
+    int size();
+
+    byte[] toByteArray();
+
+    ByteBuffer toByteBuffer();
+
+    InputStream newInputStream();
+
+    String toString(Charset charset);
+}
