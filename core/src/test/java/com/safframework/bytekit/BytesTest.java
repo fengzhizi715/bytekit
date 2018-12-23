4cec51a9474b749cb002692070d59bd611d003d0
fengzhizi715 <fengzhizi715@126.com>
2018-12-24 01:26:08 +0800

    update BytesTest

--- core/src/test/java/com/safframework/bytekit/BytesTest.java
+++ core/src/test/java/com/safframework/bytekit/BytesTest.java
@@ -1,6 +1,7 @@
 package com.safframework.bytekit;
 
 import com.safframework.bytekit.domain.User;
+import com.safframework.bytekit.jdk.ByteArrayBytes;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
@@ -10,6 +11,23 @@ import static junit.framework.TestCase.assertNotSame;
  * Created by tony on 2018-12-24.
  */
 public class BytesTest {
+
+    @Test
+    public void testHexString() {
+
+        Bytes bytes = ByteArrayBytes.create("hello world");
+        assertEquals("68656c6c6f20776f726c64", bytes.toHexString());
+    }
+
+    @Test
+    public void testHash() {
+
+        Bytes bytes = ByteArrayBytes.create("hello world");
+
+        assertEquals("5eb63bbbe01eeed093cb22bb8f5acdc3", bytes.md5().toHexString());
+        assertEquals("2aae6c35c94fcfb415dbe95f408b9ce91ee846ed", bytes.sha1().toHexString());
+        assertEquals("b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9", bytes.sha256().toHexString());
+    }
 
     @Test
     public void testSerializeAndDeserialize() {
