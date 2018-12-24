cb372d79cee6a06a1dbf5174914edcbd72511fed
fengzhizi715 <fengzhizi715@126.com>
2018-12-24 13:31:32 +0800

    add MmapBytesTest

---
+++ core/src/test/java/com/safframework/bytekit/MmapBytesTest.java
@@ -0,0 +1,61 @@
+package com.safframework.bytekit;
+
+import com.safframework.bytekit.domain.User;
+import com.safframework.bytekit.jdk.mmap.MmapBytes;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by tony on 2018-12-24.
+ */
+public class MmapBytesTest {
+
+    private MmapBytes mmapBytes;
+
+    @Before
+    public void setUp() {
+        String file = "test";
+        mmapBytes = new MmapBytes(file, (long) 1024 * 10); // 10M
+    }
+
+    @Test
+    public void testWriteAndRead() throws Exception {
+
+        mmapBytes.writeInt(12);
+        mmapBytes.writeInt(34);
+        mmapBytes.writeByte((byte) 5);
+        mmapBytes.writeBytes(("this is tony").getBytes());
+        mmapBytes.writeLong(6666L);
+        mmapBytes.writeDouble(3.14d);
+
+        assertEquals(12, mmapBytes.readInt());
+        assertEquals(34, mmapBytes.readInt());
+        assertEquals((byte) 5, mmapBytes.readByte());
+        assertEquals("this is tony", new String(mmapBytes.readBytes(12)));
+        assertEquals(6666L, mmapBytes.readLong());
+        assertEquals(3.14d, mmapBytes.readDouble());
+    }
+
+    @Test
+    public void testObject() throws Exception {
+
+        User u = new User();
+        u.name = "tony";
+        u.password = "123456";
+
+        mmapBytes.writeObject(u);
+
+        User temp = (User)mmapBytes.readObject(117);
+
+        assertEquals(u.name, temp.name);
+        assertEquals(u.password, temp.password);
+    }
+
+    @After
+    public void tearDown() {
+        mmapBytes.free();
+    }
+}
