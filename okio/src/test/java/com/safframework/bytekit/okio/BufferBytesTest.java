ecd3894d5c2a17d3294aeb98db4211e978c6dd25
fengzhizi715 <fengzhizi715@126.com>
2019-02-26 15:16:14 +0800

    add BufferBytesTest

---
+++ okio/src/test/java/com/safframework/bytekit/okio/BufferBytesTest.java
@@ -0,0 +1,65 @@
+package com.safframework.bytekit.okio;
+
+import com.safframework.bytekit.Bytes;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by tony on 2019-02-26.
+ */
+public class BufferBytesTest {
+
+    @Test
+    public void testContact() {
+
+        Bytes bytes = new ByteStringBytes("hello world".getBytes()).contact(" tony".getBytes());
+
+        assertEquals(bytes.toString(), "hello world tony");
+    }
+
+    @Test
+    public void testCopy() {
+
+        Bytes bytes = new ByteStringBytes("hello world".getBytes()).contact(" tony".getBytes());
+
+        assertEquals(bytes.toString(), bytes.copy().toString());
+    }
+
+    @Test
+    public void testReverse() {
+
+        Bytes bytes = new ByteStringBytes("hello world".getBytes()).contact(" tony".getBytes());
+
+        assertEquals(bytes.toString(), bytes.reverse().reverse().toString());
+    }
+
+    @Test
+    public void testBitWise() {
+
+        ByteStringBytes bytes = (ByteStringBytes)new ByteStringBytes("hello world".getBytes());
+        bytes.contact(" tony".getBytes());
+
+        assertEquals(bytes.toString(), bytes.and(bytes.toByteArray()).or(bytes.toByteArray()).toString());
+        assertEquals(bytes.toString(), bytes.not(bytes.toByteArray()).not(bytes.toByteArray()).toString());
+        assertEquals(bytes.toString(), bytes.xor(bytes.toByteArray()).xor(bytes.toByteArray()).toString()); //两次xor 返回本身
+    }
+
+    @Test
+    public void testBase64() {
+
+        ByteStringBytes bytes = new ByteStringBytes("hello world".getBytes());
+        bytes.contact(" tony".getBytes());
+
+        String base64 = new String(bytes.encodeBase64());
+        assertEquals(bytes.toString(), new String(Bytes.parseBase64(base64)));
+    }
+
+    @Test
+    public void testToByteArray() {
+
+        Bytes bytes = new ByteStringBytes("hello world".getBytes()).contact(" tony".getBytes());
+
+        assertEquals(bytes.toString(), new String(bytes.toByteArray()));
+    }
+}
